2cb112205f1d80dc0861a24d7ee261901dead944
LucasLuis-Dev <lucasluisouza@gmail.com>
2024-06-23 20:26:09 -0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+  import { RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  <header>
+    <img alt="InvestMap logo" class="logo" src="@/assets/images/logo.png" />
+
+    <ul class="navbar-list">
+      <li>ooj</li>
+      <li>d3</li>
+      <li>22</li>
+    </ul>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped lang="scss">
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 2% 8%;
+  line-height: 1.5;
+  height: fit-content;
+  width: 100vw;
+
+  & .logo {
+    width: 15%;
+  }
+
+  & .navbar-list {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    list-style: none;
+  }
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <main>
+    Olá
+  </main>
+</template>
